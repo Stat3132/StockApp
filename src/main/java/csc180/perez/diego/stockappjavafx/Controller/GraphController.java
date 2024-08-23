1c22c96d9ed4f183011c7bf75cec30bfd50fa2fe
dailyonpontious <161069854+dailyonpontious@users.noreply.github.com>
2024-08-22 18:31:25 -0600

    Add HistoricalChart view and GraphController
    Created a new HistoricalChart.fxml file to add a historical data chart view. Introduced GraphController for handling chart data retrieval and display operations. Also, updated HelloApplication to load this new view and commented out the initial database and API access methods.

---
+++ src/main/java/csc180/perez/diego/stockappjavafx/Controller/GraphController.java
@@ -0,0 +1,132 @@
+/**
+ * @author dpontious
+ * @createdOn 8/21/2024 at 6:18 PM
+ * @projectName JavaFx
+ * @packageName csc180.perez.diego.stockappjavafx.Controller;
+ */
+package csc180.perez.diego.stockappjavafx.Controller;
+
+import csc180.perez.diego.stockappjavafx.Model.Stock;
+import csc180.perez.diego.stockappjavafx.UTIL.StockTicker;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+public class GraphController {
+    @FXML
+    public TextField txtFieldSearch;
+
+    @FXML
+    private Button btn1Month;
+
+    @FXML
+    private Button btn1Week;
+
+    @FXML
+    private Button btn1Year;
+
+    @FXML
+    private Button btn2Years;
+
+    @FXML
+    private Button btn3Month;
+
+    @FXML
+    private Button btn6Month;
+
+    @FXML
+    private Button btnExit;
+
+    @FXML
+    private LineChart<String, BigInteger> chartHistoricalData;
+
+    @FXML
+    void exit(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oneMonth(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oneWeek(ActionEvent event) {
+
+    }
+
+    @FXML
+    void oneYear(ActionEvent event) {
+        StockTicker stockTicker = getSearchStock();
+        bulidHistoricalDataChart(stockTicker, "year", 1);
+    }
+
+    @FXML
+    void sixMonths(ActionEvent event) {
+        StockTicker stockTicker = getSearchStock();
+        bulidHistoricalDataChart(stockTicker, "month", 6);
+    }
+
+    @FXML
+    void threeMonths(ActionEvent event) {
+        StockTicker stockTicker = getSearchStock();
+        bulidHistoricalDataChart(stockTicker, "month", 3);
+    }
+
+    @FXML
+    void twoYear(ActionEvent event) {
+        StockTicker stockTicker = getSearchStock();
+        bulidHistoricalDataChart(stockTicker, "year", 2);
+    }
+
+    private void bulidHistoricalDataChart(StockTicker stockTicker, String timespan, int timePeriodBefore){
+        chartHistoricalData.getData().clear();
+        StockApi.accessingAPI(stockTicker, timespan, timePeriodBefore);
+        Map<String, List<Map<String, Stock>>> histMapOfStockValues = StockApi.getHistoricalStockValueMap();
+//        CategoryAxis xAxis = new CategoryAxis();
+//        NumberAxis yAxis = new NumberAxis();
+//        xAxis.setLabel("Period");
+//        yAxis.setLabel("Price");
+        XYChart.Series<String,BigInteger> series = new XYChart.Series<>();
+        int period = 0;
+        for(Map.Entry<String, List<Map<String, Stock>>> entry : histMapOfStockValues.entrySet()){
+            for(Map<String, Stock> stockData : entry.getValue()){
+                for(Map.Entry<String, Stock> stock : stockData.entrySet()){
+                    ++period;
+                    BigDecimal decimalOfVolume = BigDecimal.valueOf(stock.getValue().getVolume());
+                    BigInteger bigIntegerOfVolume = decimalOfVolume.toBigInteger();
+                    series.getData().add(new XYChart.Data<>("" + period, bigIntegerOfVolume));
+                    //System.out.println(bigIntegerOfVolume);
+                }
+            }
+        }
+        chartHistoricalData.getData().add(series);
+        StockApi.clearHistoricalStockValueMap();
+    }
+
+    private StockTicker getSearchStock(){
+        String stockToSearch = txtFieldSearch.getText();
+       ArrayList<StockTicker> enumOfStocks = new ArrayList<>(Arrays.asList(StockTicker.values()));
+       for(StockTicker stock : enumOfStocks){
+           if(stock.toString().equals(stockToSearch)){
+               return stock;
+           }
+       }
+       return StockTicker.NVDA;
+    }
+
+}
+
+
--- src/main/java/csc180/perez/diego/stockappjavafx/HelloApplication.java
+++ src/main/java/csc180/perez/diego/stockappjavafx/HelloApplication.java
@@ -12,16 +12,16 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("StartingScreen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("HistoricalChart.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Stock Application!");
         stage.setScene(scene);
         stage.show();
     }
     public static void main(String[] args) {
-        DatabaseController.createDatabase();
-        StockApi api = new StockApi();
-        api.accessingAPI();
+        //DatabaseController.createDatabase();
+        //StockApi api = new StockApi();
+        //api.accessingAPI();
         launch();
     }
 }
